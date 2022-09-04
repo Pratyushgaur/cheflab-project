41d31a1667fef00c7c5be98e9f9c5ac430055e0d
pratyush gaur <pratyushgaur07@gmail.com>
2022-09-04 19:51:40 +0530

    city map functionality

--- resources/views/admin/city/city.blade.php
+++ resources/views/admin/city/city.blade.php
@@ -45,10 +45,26 @@
 							</div>
 							<div class="card-body">
 							  <div class="form-group">
-								<label for="city_name">City</label>
-								<input type="text" id="city_name" name="city_name" class="form-control" placeholder="City Name">
-								<input type="hidden" name="txtpkey" id="txtpkey" value="{{!empty($city_data[0]->id) ? $city_data[0]->id : ''}}">
+                  <label for="city_name">City</label>
+                  <input type="text" id="city_name" name="city_name" class="form-control" placeholder="City Name">
+                  <input type="hidden" name="txtpkey" id="txtpkey" value="{{!empty($city_data[0]->id) ? $city_data[0]->id : ''}}">
 							  </div>
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="location">Enter location <span class="error_validate">*</span></label>
+                        <input type="text" id="address-input" name="location" class="form-control map-input" value="{{ old('location') }}" >
+                        <input type="hidden" name="address_latitude" id="address-latitude" value="{{ old('address_latitude') ?? '0' }}" />
+                        <input type="hidden" name="address_longitude" id="address-longitude" value="{{ old('address_longitude') ?? '0' }}" />
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <div id="address-map-container" style="width:100%;height:400px; ">
+                            <div style="width: 100%; height: 100%" id="address-map"></div>
+                        </div>
+                    </div>
+
+                </div>
 							</div>
 							<!-- /.card-body -->
 						</div>
@@ -107,6 +123,7 @@
 @section('js_section')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/additional-methods.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&libraries=places&callback=initialize&language=en&region=GB" async defer></script>
 <script type="text/javascript">
     $(".s_meun").removeClass("active");
     $(".city_cityadmin").addClass("active");
@@ -147,6 +164,99 @@
   function reload_table() {
       table.DataTable().ajax.reload(null, false);
    }
+   function initialize() {
+
+            $('form').on('keyup keypress', function(e) {
+                var keyCode = e.keyCode || e.which;
+                if (keyCode === 13) {
+                    e.preventDefault();
+                    return false;
+                }
+            });
+            const locationInputs = document.getElementsByClassName("map-input");
+
+            const autocompletes = [];
+            const geocoder = new google.maps.Geocoder;
+            for (let i = 0; i < locationInputs.length; i++) {
+
+                const input = locationInputs[i];
+                const fieldKey = input.id.replace("-input", "");
+                const isEdit = document.getElementById(fieldKey + "-latitude").value != '' && document.getElementById(fieldKey + "-longitude").value != '';
+
+                const latitude = parseFloat(document.getElementById(fieldKey + "-latitude").value) || 51.5073509;
+                const longitude = parseFloat(document.getElementById(fieldKey + "-longitude").value) || -0.12775829999998223;
+
+                const map = new google.maps.Map(document.getElementById(fieldKey + '-map'), {
+                    center: {
+                        lat: latitude,
+                        lng: longitude
+                    },
+                    zoom: 13
+                });
+                const marker = new google.maps.Marker({
+                    map: map,
+                    position: {
+                        lat: latitude,
+                        lng: longitude
+                    },
+                });
+
+                marker.setVisible(isEdit);
+
+                const autocomplete = new google.maps.places.Autocomplete(input);
+                autocomplete.key = fieldKey;
+                autocompletes.push({
+                    input: input,
+                    map: map,
+                    marker: marker,
+                    autocomplete: autocomplete
+                });
+            }
+
+            for (let i = 0; i < autocompletes.length; i++) {
+                const input = autocompletes[i].input;
+                const autocomplete = autocompletes[i].autocomplete;
+                const map = autocompletes[i].map;
+                const marker = autocompletes[i].marker;
+
+                google.maps.event.addListener(autocomplete, 'place_changed', function() {
+                    marker.setVisible(false);
+                    const place = autocomplete.getPlace();
+
+                    geocoder.geocode({
+                        'placeId': place.place_id
+                    }, function(results, status) {
+                        if (status === google.maps.GeocoderStatus.OK) {
+                            const lat = results[0].geometry.location.lat();
+                            const lng = results[0].geometry.location.lng();
+                            setLocationCoordinates(autocomplete.key, lat, lng);
+                        }
+                    });
+
+                    if (!place.geometry) {
+                        window.alert("No details available for input: '" + place.name + "'");
+                        input.value = "";
+                        return;
+                    }
+
+                    if (place.geometry.viewport) {
+                        map.fitBounds(place.geometry.viewport);
+                    } else {
+                        map.setCenter(place.geometry.location);
+                        map.setZoom(17);
+                    }
+                    marker.setPosition(place.geometry.location);
+                    marker.setVisible(true);
+
+                });
+            }
+    }
+    function setLocationCoordinates(key, lat, lng) {
+        const latitudeField = document.getElementById(key + "-" + "latitude");
+        const longitudeField = document.getElementById(key + "-" + "longitude");
+        latitudeField.value = lat;
+        longitudeField.value = lng;
+    }
 
  </script>
 @endsection
