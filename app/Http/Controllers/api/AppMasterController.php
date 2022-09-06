c40152fabc125caf13cac9e8f4aa01e7d69a54e0
VIKAS092 <vikasrathor529@gmail.com>
2022-09-06 17:24:18 +0530

    test commit

--- app/Http/Controllers/api/AppMasterController.php
+++ app/Http/Controllers/api/AppMasterController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+// this is vikas testing
 class AppMasterController extends Controller
 {
     public function getCategories()
