361e1162d07414479de39c642e5f71977627fece
pratyush gaur <pratyushgaur07@gmail.com>
2022-09-23 18:14:16 +0530

    add vendor model

---
+++ app/Models/Vendors.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
+
+
+class Vendors extends Authenticatable
+{
+    use HasFactory, Notifiable;
+    protected $fillable = ['table_service'];
+
+    public function vendor_order_time()
+    {
+        $this->hasMany(Order_time::class);
+    }
+
+    static function is_avaliavle($vendor_id)
+    {
+//        date_default_timezone_set(config('app.timezone'));
+        $v = Vendors::find($vendor_id);
+        if (!$v->is_online)
+            return ['offline'];
+        return \App\Models\Order_time::where('vendor_id',$vendor_id)
+            ->where('day_no',Carbon::now()->dayOfWeek)
+            ->where('start_time','<=',date('H:i:s'))
+            ->where('end_time','>',date('H:i:s'))
+            ->exists();
+    }
+
+
+}
