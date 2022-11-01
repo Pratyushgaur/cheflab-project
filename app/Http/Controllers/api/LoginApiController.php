1666f1f35b846f5751ef1477a1fe5df263826afa
pratyush gaur <pratyushgaur07@gmail.com>
2022-11-01 10:53:29 +0530

    api register user

--- app/Http/Controllers/api/LoginApiController.php
+++ app/Http/Controllers/api/LoginApiController.php
@@ -118,7 +118,7 @@ class LoginApiController extends Controller
 
             //check otp is verified
             if(Mobileotp::where(['mobile_number' =>$request->mobile_number,'status' =>'1'])->exists()){
-                $refer_amount = AdminMasters::select('refer_amount');
+              //  $refer_amount = AdminMasters::select('refer_amount');
                 $user = new User;
                 if($request->referralcode != ''){//
 
@@ -139,7 +139,7 @@ class LoginApiController extends Controller
                 $user->name =$request->name;
                 $user->email =$request->email;
                 $user->mobile_number =$request->mobile_number;
-                $user->by_earn = $refer_amount;
+              //  $user->by_earn = $refer_amount;
                 $user->alternative_number =$request->alternative_mobile;
                 $user->referralCode = $this->generateReferralCode($request->name);
                 $user->save();
