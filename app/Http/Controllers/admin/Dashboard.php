02912b372c2f4b5ff036f03af755d8400bd90911
pratyush gaur <pratyushgaur07@gmail.com>
2022-11-03 19:38:06 +0530

    shivanir api

--- app/Http/Controllers/admin/Dashboard.php
+++ app/Http/Controllers/admin/Dashboard.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\admin;
 use App\Models\MangaoStaticUseradmin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Vendors;
+use App\Models\Deliver_boy;
+use App\Models\Orders;
 use Illuminate\Support\Facades\Gate;
 use App\Models\Notification;
 use Auth;
@@ -40,7 +43,14 @@ class Dashboard extends Controller
         //     }
 
         // die();
-      
-        return view('admin.dashbord.dash');
+        $wordlist = Orders::where('payment_status', '=','paid')->get();
+        $deliveryorder = $wordlist->count();
+        $re_avtive = Orders::where('order_status', '=','restaurant')->where('status', '=','1')->get();
+        $active_resto = $re_avtive->count();
+        $delivery = Deliver_boy::all();
+        $delivery_boy = $delivery->count();
+        $chef = Vendors::where('vendor_type', '=','chef')->get();
+        $chef = $chef->count();
+        return view('admin.dashbord.dash', compact('deliveryorder','active_resto','delivery_boy','chef'));
     }
 }
