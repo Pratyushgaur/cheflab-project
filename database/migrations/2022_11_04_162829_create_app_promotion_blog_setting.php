f05db8e4491be790ad99c49922dc5259956a269c
pratyush gaur <pratyushgaur07@gmail.com>
2022-11-04 16:33:22 +0530

    promotion bolg migration

---
+++ database/migrations/2022_11_04_162829_create_app_promotion_blog_setting.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppPromotionBlogSetting extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_promotion_blog_setting', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('app_promotion_blog_id');
+            $table->tinyInteger('blog_position');
+            $table->string('blog_price');
+            $table->string('blog_promotion_date_frame')->comment('week,2 week, one month');
+            $table->enum('is_active', ['1', '0'])->default('1')->comment('1-active 0-inactive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_promotion_blog_setting');
+    }
+}
