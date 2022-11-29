398b1734b4c62d4724989887685edd6502a1d8d9
pratyush gaur <pratyushgaur07@gmail.com>
2022-11-29 17:37:13 +0530

    star remove in product craete and update

--- resources/views/vendor/restaurant/products/create.blade.php
+++ resources/views/vendor/restaurant/products/create.blade.php
@@ -139,8 +139,7 @@
 
 
                                 <div class="col-md-12 mb-3">
-                                    <label for="validationCustom12">Description <span
-                                            class="text-danger">*</span></label>
+                                    <label for="validationCustom12">Description</label>
                                     <div class="input-group">
                                         <textarea rows="5" id="validationCustom12" name="dis" class="form-control"
                                                   placeholder="Message"></textarea>
