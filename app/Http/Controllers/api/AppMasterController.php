d06fbea9031c54083534954acc8b1f27fa2f7a93
pratyush gaur <pratyushgaur07@gmail.com>
2022-12-06 09:43:20 +0530

    browse menu bug solve

--- app/Http/Controllers/api/AppMasterController.php
+++ app/Http/Controllers/api/AppMasterController.php
@@ -9,77 +9,77 @@ use Validator;
 // this is vikas testing
 class AppMasterController extends Controller
 {
-    public function getCategories(Request $request)
-    {
-        try {
-            $validateUser = Validator::make(
-                $request->all(),
-                [
-                    'lat' => 'required|numeric',
-                    'lng' => 'required|numeric'
-                ]
-            );
-            if ($validateUser->fails()) {
-                $error = $validateUser->errors();
-                return response()->json([
-                    'status' => false,
-                    'error'  => $validateUser->errors()->all()
+    // public function getCategories(Request $request)
+    // {
+    //     try {
+    //         $validateUser = Validator::make(
+    //             $request->all(),
+    //             [
+    //                 'lat' => 'required|numeric',
+    //                 'lng' => 'required|numeric'
+    //             ]
+    //         );
+    //         if ($validateUser->fails()) {
+    //             $error = $validateUser->errors();
+    //             return response()->json([
+    //                 'status' => false,
+    //                 'error'  => $validateUser->errors()->all()
 
-                ], 401);
-            }
+    //             ], 401);
+    //         }
 
-            $resturants   = get_restaurant_ids_near_me($request->lat, $request->lng, ['vendor_type' => 'restaurant'], true, null, null, false);
-            $category_ids = $resturants->join('products as p', 'p.userId', '=', 'vendors.id')
-                ->where('p.status','=','1')
-                ->where('p.product_approve','=','1')
-                ->addSelect('p.category', \DB::raw('COUNT(*) as product_count'))
-                ->groupBy('p.category')->having('product_count', '>',0)->pluck('p.category');
+    //         $resturants   = get_restaurant_ids_near_me($request->lat, $request->lng, ['vendor_type' => 'restaurant'], true, null, null, false);
+    //         $category_ids = $resturants->join('products as p', 'p.userId', '=', 'vendors.id')
+    //             ->where('p.status','=','1')
+    //             ->where('p.product_approve','=','1')
+    //             ->addSelect('p.category', \DB::raw('COUNT(*) as product_count'))
+    //             ->groupBy('p.category')->having('product_count', '>',0)->pluck('p.category');
 
-            if ($category_ids != '' && $category_ids != null){
-                $vendorCatIds = [];
-                foreach ($category_ids as $ids){
-                    $vendor_count = get_restaurant_near_me($request->lat, $request->lng, ['vendor_type' => 'restaurant'], null, null, null)
-                    ->whereRaw('FIND_IN_SET(' . $ids . ',deal_categories)')->count();
-                    if($vendor_count > 0){
-                        $vendorCatIds[] = $ids;
-                    }  
-                }
-                if (!empty($vendorCatIds)) {
-                    $data = \App\Models\Catogory_master::whereIn('id', $vendorCatIds)->where(['is_active' => '1'])->select('categories.name', \DB::raw('CONCAT("' . asset('categories') . '/", categoryImage) AS image'), 'id')
-                    ->orderBy('position', 'ASC')->get();
-                } else {
-                    $data = [];
-                }
+    //         if ($category_ids != '' && $category_ids != null){
+    //             $vendorCatIds = [];
+    //             foreach ($category_ids as $ids){
+    //                 $vendor_count = get_restaurant_near_me($request->lat, $request->lng, ['vendor_type' => 'restaurant'], null, null, null)
+    //                 ->whereRaw('FIND_IN_SET(' . $ids . ',deal_categories)')->count();
+    //                 if($vendor_count > 0){
+    //                     $vendorCatIds[] = $ids;
+    //                 }  
+    //             }
+    //             if (!empty($vendorCatIds)) {
+    //                 $data = \App\Models\Catogory_master::whereIn('id', $vendorCatIds)->where(['is_active' => '1'])->select('categories.name', \DB::raw('CONCAT("' . asset('categories') . '/", categoryImage) AS image'), 'id')
+    //                 ->orderBy('position', 'ASC')->get();
+    //             } else {
+    //                 $data = [];
+    //             }
 
-                return response()->json([
-                    'status'   => true,
-                    'message'  => 'Data Get Successfully',
-                    'response' => $data
+    //             return response()->json([
+    //                 'status'   => true,
+    //                 'message'  => 'Data Get Successfully',
+    //                 'response' => $data
 
-                ], 200);
-            }
-            else{
-                $data = [];
-                return response()->json([
-                    'status'   => true,
-                    'message'  => 'Data Get Successfully',
-                    'response' => $data
+    //             ], 200);
+    //         }
+    //         else{
+    //             $data = [];
+    //             return response()->json([
+    //                 'status'   => true,
+    //                 'message'  => 'Data Get Successfully',
+    //                 'response' => $data
 
-                ], 200);
-            }
+    //             ], 200);
+    //         }
                 
                 
             
 
 
-        } catch (\Throwable $th) {
-            return response()->json([
-                'status' => false,
-                'error'  => $th->getMessage()
-            ], 500);
-        }
+    //     } catch (\Throwable $th) {
+    //         return response()->json([
+    //             'status' => false,
+    //             'error'  => $th->getMessage()
+    //         ], 500);
+    //     }
 
-    }
+    // }
 
     // public function getCuisines(Request $request)
     // {
@@ -172,6 +172,56 @@ class AppMasterController extends Controller
             foreach ($cuisines as $key => $value) {
                 $resturants   = get_restaurant_ids_near_me($request->lat, $request->lng, ['vendor_type' => 'restaurant'], true, null, null, false);
                 $resturants = $resturants->whereRaw('FIND_IN_SET(' . $value->id . ',deal_cuisines)');
+                $resturants = $resturants->join('products as p', 'p.userId', '=', 'vendors.id');
+                $resturants = $resturants->where('p.status','=','1');
+                $resturants = $resturants->addSelect( \DB::raw('COUNT(*) as product_count'),'vendors.id');
+                $resturants = $resturants->groupBy('p.id')->having('product_count', '>',0);
+                if($resturants->get()->count('vendors.id') > 0){
+                    $data[] = $value;
+                }
+            }
+            return response()->json([
+                'status'   => true,
+                'message'  => 'Data Get Successfully',
+                'response' => $data
+
+            ], 200);
+            
+
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'error'  => $th->getTrace()
+            ], 500);
+        }
+
+    }
+    public function getCategories(Request $request)
+    {
+        try {
+            $validateUser = Validator::make(
+                $request->all(),
+                [
+                    'lat' => 'required|numeric',
+                    'lng' => 'required|numeric'
+                ]
+            );
+            if ($validateUser->fails()) {
+                $error = $validateUser->errors();
+                return response()->json([
+                    'status' => false,
+                    'error'  => $validateUser->errors()->all()
+
+                ], 401);
+            }
+
+
+            $category = \App\Models\Catogory_master::where('is_active','=','1')->orderBy('position','ASC')->select('categories.name', \DB::raw('CONCAT("' . asset('categories') . '/", categoryImage) AS image'), 'id','position')->get();
+            $data = [];
+            foreach ($category as $key => $value) {
+                $resturants   = get_restaurant_ids_near_me($request->lat, $request->lng, ['vendor_type' => 'restaurant'], true, null, null, false);
+                $resturants = $resturants->whereRaw('FIND_IN_SET(' . $value->id . ',deal_categories)');
                 $resturants = $resturants->join('products as p', 'p.userId', '=', 'vendors.id');
                 $resturants = $resturants->where('p.status','=','1');
                 $resturants = $resturants->addSelect( \DB::raw('COUNT(*) as product_count'),'vendors.id');
