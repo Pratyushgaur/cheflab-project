b462889b2b0eb858df7ed1eb7bebf4f4a0df3681
pratyush gaur <pratyushgaur07@gmail.com>
2022-12-24 19:23:05 +0530

    guzzle use for hit url

--- app/helpers.php
+++ app/helpers.php
@@ -3,6 +3,7 @@
 use App\Models\Orders;
 use App\Models\Product_master;
 use Carbon\Carbon;
+use GuzzleHttp\Client;
 
 function time_diffrence_in_minutes($datetime1, $datetime2)
 {
@@ -751,20 +752,15 @@ function getDrivingDistance($lat1, $long1, $lat2, $long2)
 {
     $key = env('GOOGLE_MAPS_API_KEY');
     $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" . $lat1 . "," . $long1 . "&destinations=" . $lat2 . "," . $long2 . "&mode=driving&language=pl-PL&key=$key";
-    $ch  = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    $response = curl_exec($ch);
-    curl_close($ch);
-    $response_a = json_decode($response, true);
+    $client = new Client();
+    $response = $client->request('GET', $url);
+    $d = $response->getBody();
+    $response_a = json_decode($d, true);
     $dist       = $response_a['rows'][0]['elements'][0]['distance']['text'];
     $time       = $response_a['rows'][0]['elements'][0]['duration']['text'];
     $dist       = str_replace(',', '.', $dist);
     return $dist = str_replace('km', '', $dist);
-    //return array('distance' => $dist, 'time' => $time);
+    return array('distance' => $dist, 'time' => $time);
 }
 
 function getOrderId()
