3769834612f660749ee9c5507deeb1ca3180dfdd
pratyush gaur <pratyushgaur07@gmail.com>
2022-12-30 17:18:39 +0530

    rider assign code bug solve -3

--- app/Http/Controllers/api/rider/AppController.php
+++ app/Http/Controllers/api/rider/AppController.php
@@ -252,16 +252,17 @@ class AppController extends Controller
             }
             if($order->otp == $request->otp){
                 RiderAssignOrders::where('id','=',$order->id)->update('action','4');
-                $order = RiderAssignOrders::where('rider_assign_orders.id','=',$request->rider_assign_orders);
+                Order::where('id','=',$request->order_row_id)->update(['order_status'=>'dispatched']);
+                $order = RiderAssignOrders::where('rider_assign_orders.id','=',$request->rider_assign_order_id);
                 $order = $order->join('orders','rider_assign_orders.order_id','=','orders.id');
                 $order = $order->join('vendors','orders.vendor_id','=','vendors.id');
-                $order = $order->select('vendors.name as vendor_name','vendors.address as vendor_address','orders.customer_name','orders.delivery_address',DB::raw('if(rider_assign_orders.action = "1", "accepted", "pending")  as rider_status'),'action','orders.id as order_row_id','orders.order_id');
+                $order = $order->select('vendors.name as vendor_name','vendors.address as vendor_address','orders.customer_name','orders.delivery_address',DB::raw('if(rider_assign_orders.action = "1", "accepted", "pending")  as rider_status'),'action','orders.id as order_row_id','orders.order_id','rider_assign_orders.id as rider_assign_order_id');
                 $order = $order->addSelect('vendors.mobile as vendor_mobile','vendors.lat as vendor_lat','vendors.long as vendor_lng','orders.lat as customer_lat','orders.long as customer_lng','orders.mobile_number as customer_mobile','net_amount',);
                 $order = $order->leftJoin('order_products', 'orders.id', '=', 'order_products.order_id');
                 $order = $order->first();
                 $order->products = OrderProduct::where('order_id','=',$order->order_row_id)->join('products','order_products.product_id','=','products.id')->leftJoin('order_product_variants','order_products.id','=','order_product_variants.order_product_id')->select('order_products.product_name','order_product_variants.variant_name','products.type')->get();
                 //
-                Order::where('id','=',$request->order_row_id)->update(['order_status'=>'dispatched']);
+                
                 return response()->json([
                     'status'   => true,
                     'message'  => 'Status Updated Successfully'
