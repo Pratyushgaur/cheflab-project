95622895fb0c7b2dfcdf4292f144ef0aeed3ef3d
pratyush gaur <pratyushgaur07@gmail.com>
2022-12-30 18:02:57 +0530

    rider assign code bug solve -4

--- app/Http/Controllers/api/rider/AppController.php
+++ app/Http/Controllers/api/rider/AppController.php
@@ -41,7 +41,7 @@ class AppController extends Controller
             $order = RiderAssignOrders::where(['rider_id' =>$request->user_id,'action' =>'0'])->orWhere(['rider_id' =>$request->user_id,'action' =>'1'])->orderBy('rider_assign_orders.id','desc')->limit(1);
             $order = $order->join('orders','rider_assign_orders.order_id','=','orders.id');
             $order = $order->join('vendors','orders.vendor_id','=','vendors.id');
-            $order = $order->select('vendors.name as vendor_name','vendors.address as vendor_address','orders.customer_name','orders.delivery_address',DB::raw('if(rider_assign_orders.action = "1", "accepted", "pending")  as rider_status'),'action','orders.id as order_row_id','orders.order_id','rider_assign_orders.id as rider_assign_order_id');
+            $order = $order->select('vendors.name as vendor_name','vendors.address as vendor_address','orders.order_status','orders.customer_name','orders.delivery_address',DB::raw('if(rider_assign_orders.action = "1", "accepted", "pending")  as rider_status'),'action','orders.id as order_row_id','orders.order_id','rider_assign_orders.id as rider_assign_order_id');
            
             if ($order->first()->action == '0') {
                 $order = $order->first();
@@ -193,7 +193,7 @@ class AppController extends Controller
                 $order = $order->join('orders','rider_assign_orders.order_id','=','orders.id');
                 $order = $order->join('vendors','orders.vendor_id','=','vendors.id');
                 $order = $order->select('vendors.name as vendor_name','vendors.address as vendor_address','orders.customer_name','orders.delivery_address',DB::raw('if(rider_assign_orders.action = "1", "accepted", "pending")  as rider_status'),'action','orders.id as order_row_id','orders.order_id','rider_assign_orders.id as rider_assign_order_id');
-                $order = $order->addSelect('vendors.mobile as vendor_mobile','vendors.lat as vendor_lat','vendors.long as vendor_lng','orders.lat as customer_lat','orders.long as customer_lng','orders.mobile_number as customer_mobile','net_amount',);
+                $order = $order->addSelect('vendors.mobile as vendor_mobile','orders.order_status','vendors.lat as vendor_lat','vendors.long as vendor_lng','orders.lat as customer_lat','orders.long as customer_lng','orders.mobile_number as customer_mobile','net_amount',);
                 $order = $order->leftJoin('order_products', 'orders.id', '=', 'order_products.order_id');
                 $order = $order->first();
                 $order->products = OrderProduct::where('order_id','=',$order->order_row_id)->join('products','order_products.product_id','=','products.id')->leftJoin('order_product_variants','order_products.id','=','order_product_variants.order_product_id')->select('order_products.product_name','order_product_variants.variant_name','products.type')->get();
@@ -256,7 +256,7 @@ class AppController extends Controller
                 $order = RiderAssignOrders::where('rider_assign_orders.id','=',$request->rider_assign_order_id);
                 $order = $order->join('orders','rider_assign_orders.order_id','=','orders.id');
                 $order = $order->join('vendors','orders.vendor_id','=','vendors.id');
-                $order = $order->select('vendors.name as vendor_name','vendors.address as vendor_address','orders.customer_name','orders.delivery_address',DB::raw('if(rider_assign_orders.action = "1", "accepted", "pending")  as rider_status'),'action','orders.id as order_row_id','orders.order_id','rider_assign_orders.id as rider_assign_order_id');
+                $order = $order->select('vendors.name as vendor_name','vendors.address as vendor_address','orders.order_status','orders.customer_name','orders.delivery_address',DB::raw('if(rider_assign_orders.action = "1", "accepted", "pending")  as rider_status'),'action','orders.id as order_row_id','orders.order_id','rider_assign_orders.id as rider_assign_order_id');
                 $order = $order->addSelect('vendors.mobile as vendor_mobile','vendors.lat as vendor_lat','vendors.long as vendor_lng','orders.lat as customer_lat','orders.long as customer_lng','orders.mobile_number as customer_mobile','net_amount',);
                 $order = $order->leftJoin('order_products', 'orders.id', '=', 'order_products.order_id');
                 $order = $order->first();
--- app/Http/Controllers/vendor/restaurant/OrderController.php
+++ app/Http/Controllers/vendor/restaurant/OrderController.php
@@ -104,6 +104,22 @@ class OrderController extends Controller
         $order->save();
         if($order->accepted_driver_id != null){
             \App\Models\RiderAssignOrders::where('order_id','=',$id)->where('rider_id','=',$order->accepted_driver_id)->update(['otp'=>rand(1000,9999)]);
+            $token = DeliveryBoyTokens::where('rider_id','=',$order->accepted_driver_id)->orderBy('id','desc')->get()->pluck('token');
+            if(!empty($token)){
+                $riderAssign =  \App\Models\RiderAssignOrders::where('order_id','=',$id)->where('rider_id','=',$order->accepted_driver_id);
+                $riderAssign = $riderAssign->join('orders','rider_assign_orders.order_id','=','orders.id');
+                $riderAssign = $riderAssign->join('vendors','orders.vendor_id','=','vendors.id');
+                $riderAssign = $riderAssign->select('vendors.name as vendor_name','vendors.address as vendor_address','orders.order_status','orders.customer_name','orders.delivery_address',\DB::raw('if(rider_assign_orders.action = "1", "accepted", "pending")  as rider_status'),'action','orders.id as order_row_id','orders.order_id','rider_assign_orders.id as rider_assign_order_id');
+                $riderAssign = $riderAssign->first();
+                $riderAssign->expected_earninig = 50;
+                $riderAssign->trip_distance = 7;
+
+                $title = 'Restaurant Order Ready to Dispatch';
+                $body = "Restaurant Order Ready to Dispatch";
+                $res = sendNotification($title,$body,$token,array('type'=>2,'data'=>$riderAssign));
+                var_dump($res);
+            }
+            
         }
         
         return response()->json([
