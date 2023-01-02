5a4b80c85eeee3d6484c5c8900499be30babddb5
pratyush gaur <pratyushgaur07@gmail.com>
2023-01-02 16:58:08 +0530

    order accept notification true 3

--- app/Http/Controllers/api/rider/AppController.php
+++ app/Http/Controllers/api/rider/AppController.php
@@ -291,7 +291,7 @@ class AppController extends Controller
                 RiderAssignOrders::where('id','=',$order->id)->update(['action'=>'4']);
                 $orderdata = Order::where('id','=',$request->order_row_id);
                 $orderdata->update(['order_status'=>'dispatched','pickup_time'=>mysql_date_time()]);
-                $user = \App\Models\User::where('id','=',$$orderdata->user_id);
+                $user = \App\Models\User::where('id','=',$$orderdata->first()->user_id)->select('fcm_token')->first();
                 if(!empty($user)){
                     if($user->fcm_token != ''){
                         //xsendUserAppNotification('Order dispated from restaurant ',"Your Order has been Dispatched",$user->fcm_token,array('type'=>4,'data'=>array('data'=>array())));
@@ -304,7 +304,7 @@ class AppController extends Controller
                 $order = $order->join('orders','rider_assign_orders.order_id','=','orders.id');
                 $order = $order->join('vendors','orders.vendor_id','=','vendors.id');
                 $order = $order->select('vendors.name as vendor_name','vendors.address as vendor_address','orders.order_status','orders.customer_name','orders.delivery_address',DB::raw('if(rider_assign_orders.action = "1", "accepted", "pending")  as rider_status'),'action','orders.id as order_row_id','orders.order_id','rider_assign_orders.id as rider_assign_order_id','otp');
-                $order = $order->addSelect('vendors.mobile as vendor_mobile','vendors.lat as vendor_lat','vendors.long as vendor_lng','orders.lat as customer_lat','orders.long as customer_lng','orders.mobile_number as customer_mobile','net_amount',);
+                $order = $order->addSelect('vendors.mobile as vendor_mobile','vendors.lat as vendor_lat','vendors.long as vendor_lng','orders.lat as customer_lat','orders.long as customer_lng','orders.mobile_number as customer_mobile','net_amount');
                 $order = $order->leftJoin('order_products', 'orders.id', '=', 'order_products.order_id');
                 $order = $order->first();
                 $order->products = OrderProduct::where('order_id','=',$order->order_row_id)->join('products','order_products.product_id','=','products.id')->leftJoin('order_product_variants','order_products.id','=','order_product_variants.order_product_id')->select('order_products.product_name','order_product_variants.variant_name','products.type')->get();
