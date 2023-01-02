fa900a3f084b15b734e055fe45c6a7543000c6bc
pratyush gaur <pratyushgaur07@gmail.com>
2023-01-03 01:16:02 +0530

    upload mis order commission model

---
+++ app/Models/OrderCommision.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderCommision extends Model
+{
+    use HasFactory;
+     protected $fillable=['is_approve','is_cancel','vendor_id', 'order_id','net_amount','vendor_commision','admin_commision', 'gross_revenue','additions','deductions','net_receivables','addition_tax','convenience_tax','admin_tax','tax','order_date','convenience_amount','tax_amount','admin_amount'];
+}
+
