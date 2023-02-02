d485a4193ac0a408dc95cb75c154bd1278906c60
Anil Meena <anilmp40@gmail.com>
2023-02-02 18:32:17 +0530

    Create push notificaion

--- resources/views/admin/notification/index.blade.php
+++ resources/views/admin/notification/index.blade.php
@@ -1,6 +1,63 @@
+
 @extends('admin.layouts.layoute')
 @section('content')
+@section('page-style')
+<style>
+  label.error {
+    color: #dc3545;
+    font-size: 14px;
+  }
 
+  .image-upload {
+    display: inline-block;
+    margin-right: 15px;
+    position: relative;
+  }
+
+  .image-upload>input {
+    display: none;
+  }
+
+  .upload-icon {
+    width: 150px;
+    height: 150px;
+    border: 2px solid #000;
+    border-style: dotted;
+    border-radius: 18px;
+  }
+
+
+
+  .upload-icon img {
+    width: 100px;
+    height: 100px;
+    margin: 19px;
+    cursor: pointer;
+  }
+
+
+  .upload-icon.has-img {
+    width: 150px;
+    height: 150px;
+    border: none;
+  }
+
+  .upload-icon.has-img img {
+    /*width: 100%;
+            height: auto;*/
+    width: 150px;
+    height: 150px;
+    border-radius: 18px;
+    margin: 0px;
+  }
+
+ 
+
+  .select2-selection__choice {
+    background: #007bff !important;
+  }
+</style>
+@endsection
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
@@ -23,7 +80,8 @@
         <div class="row mb-3">
 
             <div class="col-md-12">
-                <form id="restaurant-form" action="{{route('admin.category.store')}}" method="post" enctype="multipart/form-data">
+                <form id="restaurant-form" action="{{route('admin.push.notification.store')}}" method="post" enctype="multipart/form-data">
+                    <input type="hidden" name="status" value="1">
                     @if ($errors->any())
                     @foreach ($errors->all() as $error)
                     <div class="alert alert-danger">{{$error}}</div>
@@ -44,19 +102,19 @@
                                 <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="category_name">Title <span class="text-danger">*</span></label>
-                                        <input type="text" id="name" name="name" class="form-control" placeholder="Enter Title">
+                                        <input type="text" name="title" class="form-control" placeholder="Enter Title">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="category_name">Zone <span class="text-danger">*</span></label>
-                                        <input type="text" id="name" name="name" class="form-control" placeholder="Enter Zone">
+                                        <input type="text" name="zone" class="form-control" placeholder="Enter Zone">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="exampleInputEmail1">Send To <span class="text-danger">*</span></label>
-                                        <select class="form-control" name="discount_type" style="width: 100%;">
+                                        <select class="form-control" name="send_to" style="width: 100%;">
                                             <option value="">Select</option>
                                             <option value="1">Rider</option>
                                             <option value="2">User</option>
@@ -65,7 +123,7 @@
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-md-4">
+                                <!-- <div class="col-md-4">
                                     <div class="form-group">
                                         <div>
                                             <label for="">Notification Banner <span class="text-danger">*</span></label>
@@ -76,14 +134,27 @@
                                                     <img class="icon" src="">
                                                 </div>
                                             </label>
-                                            <input id="file-input" type="file" name="categoryImage">
+                                            <input id="file-input" type="file" name="notification_banner">
                                         </div>
+                                    </div>
+                                </div> -->
+                                <div class="col-md-4">
+                                    <div>
+                                        <label for="">Notification Banner ( Ratio 1:1 )</label>
+                                    </div>
+                                    <div class="image-upload">
+                                        <label for="file-input">
+                                            <div class="upload-icon">
+                                                <img class="icon" src="{{asset('add-image.png')}}">
+                                            </div>
+                                        </label>
+                                        <input id="file-input" type="file" name="notification_banner" required />
                                     </div>
                                 </div>
                                 <div class="col-md-8">
                                     <div class="form-group">
                                         <label for="category_name">Description<span class="text-danger">*</span></label>
-                                        <textarea id="name" name="name" class="form-control" placeholder="Description"></textarea>
+                                        <textarea id="name" name="description" class="form-control" placeholder="Description"></textarea>
                                     </div>
                                 </div>
                             </div>
@@ -124,8 +195,8 @@
                                     <th>Zone</th>
                                     <th>Send To</th>
                                     <th>Notification Banner</th>
-                                    <th>Description</th>
-                                    <th>Status</th>
+                                    <!-- <th>Description</th> -->
+                                    <!-- <th>Status</th> -->
                                 </tr>
                             </thead>
 
@@ -146,57 +217,96 @@
 @endsection
 
 @section('js_section')
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/additional-methods.js"></script>
+
+
 <script type="text/javascript">
-  window.notification_table = $('#notification_table').dataTable({
+    window.notification_table = $('#notification_table').dataTable({
 
-    dom: 'Bfrtip',
-    buttons: [{
-      extend: 'excel',
-      className: 'btn-info',
-      title: 'Account-rider'
-    }],
-    processing: true,
-    serverSide: true,
-    // buttons: true,
-    ajax: {
-      url: "{{ route('admin.notification.notification_list') }}",
-      data: function(d) {
-        // d.rider_id = $("#riderId").val()
+        dom: 'Bfrtip',
+        buttons: [{
+            extend: 'excel',
+            className: 'btn-info',
+            title: 'Account-rider'
+        }],
+        processing: true,
+        serverSide: true,
+        // buttons: true,
+        ajax: {
+            url: "{{ route('admin.notification.notification_list') }}",
+            data: function(d) {
+                // d.rider_id = $("#riderId").val()
+            }
+        },
+        columns: [{
+                data: 'DT_RowIndex',
+                name: 'DT_RowIndex'
+            },
+            {
+                data: 'title',
+                name: 'title'
+            },
+            {
+                data: 'zone',
+                name: 'zone'
+            },
+            {
+                data: 'type',
+                name: 'type'
+            },
+            {
+                data: 'image',
+                name: 'image'
+            },
+            // {
+            //     data: 'description',
+            //     name: 'description'
+            // },
+            // {
+            //     data: 'status',
+            //     name: 'status'
+            // },
+
+
+        ]
+    });
+
+
+    $('#file-input').change(function(event) {
+      $("img.icon").attr('src', URL.createObjectURL(event.target.files[0]));
+      $("img.icon").parents('.upload-icon').addClass('has-img');
+    });
+
+
+    $("#restaurant-form").validate({
+      rules: {
+        send_to: {
+          required: true,
+        },
+        zone: {
+          required: true,
+        },
+        title: {
+          required: true,
+        },
+        
+      },
+      messages: {
+        send_to: {
+          required: "Please Select User Or Rider",
+        },
+        zone: {
+          required: "Please Enter Zone",
+        },
+        title: {
+          required: "Please Enter Title",
+        },
+       
       }
-    },
-    columns: [{
-        data: 'DT_RowIndex',
-        name: 'DT_RowIndex'
-      },
-      {
-        data: 'title',
-        name: 'title'
-      },
-      {
-        data: 'zone',
-        name: 'zone'
-      },
-      {
-        data: 'send_to',
-        name: 'send_to'
-      },
-      {
-        data: 'notification_banner',
-        name: 'notification_banner'
-      },
-      {
-        data: 'description',
-        name: 'description'
-      },
-      {
-        data: 'status',
-        name: 'status'
-      },
-     
+    });
 
-    ]
-  });
-
-  </script>
+</script>
 
 @endsection
