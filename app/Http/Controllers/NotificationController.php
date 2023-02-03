bd1c6aee170c221aedbd3be00bf8ee937da9689b
Anil Meena <anilmp40@gmail.com>
2023-02-03 19:01:46 +0530

    push notificaiton image update

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -95,7 +95,7 @@ class NotificationController extends Controller
             'send_to'   => 'required',
            
         ]);
-        $notification = new pushNotification;
+        $notification = new PushNotification;
         $notification->title = $request->title;
         $notification->zone = $request->zone;
         $notification->send_to = $request->send_to;
@@ -119,12 +119,18 @@ class NotificationController extends Controller
         }
         
         $title = $request->title;
-        $body = $request->description;
+        $allBody = $request->description . " Zone " . $request->zone;
+        $body = strip_tags($allBody);
+        $image = "asset('push_notify_banner') . '/' . $filename";
+        // $image = "https://www.shutterstock.com/image-photo/rajwada-historical-palace-indore-city-260nw-567029827.jpg";
+        $data = null;
+        $sound = "default";
 
-        sendNotification($title, $body, $allToken);
+
+        sendNotification($title, $body, $allToken,$data ,$sound ,$image);
        
         return redirect()->route('admin.notification.view')->with('message', 'Notification Send To '. $send_data .' Successfully');
- 
+        
 
     }
 
