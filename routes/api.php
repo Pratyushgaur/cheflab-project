7967add53f65d944f5cf2648bc4575cee7b99bd8
RathoreManpreet <itmanpreet94@gmail.com>
2023-02-16 16:48:14 +0530

    add the test for the login history

--- routes/api.php
+++ routes/api.php
@@ -191,7 +191,7 @@ Route::post('rider-order-earnings', [App\Http\Controllers\api\rider\AppControlle
 Route::post('rider-incentive-history', [App\Http\Controllers\api\rider\AppController::class, 'incentiveHistory'])->name("rider.incentive.history");
 Route::post('rider-latLng-update', [App\Http\Controllers\api\rider\AppController::class, 'updateLatLng']);
 Route::post('rider-deliver-otp', [App\Http\Controllers\api\rider\AppController::class, 'deliverOtpCheck']);
-Route::post('rider-login-history', [App\Http\Controllers\api\rider\AppController::class, 'logInHistory']);
+Route::post('rider-login-history', [App\Http\Controllers\api\rider\AppController::class, 'logInHistory'])->name("rider.login.history");
 Route::post('rider-update-version', [\App\Http\Controllers\api\rider\LoginApiController::class, 'checkVersion'])->name("rider.update.version");
 Route::post('rider-active-status', [\App\Http\Controllers\api\rider\AppController::class, 'checkRiderActive']);
 
