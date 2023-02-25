de4500a1d2fde3c7f95e3222f210e19d9870e0b4
pratyush gaur <pratyushgaur07@gmail.com>
2023-02-25 18:08:22 +0530

    testing

--- app/Http/Controllers/api/AppController.php
+++ app/Http/Controllers/api/AppController.php
@@ -199,7 +199,7 @@ class AppController extends Controller
             $vendors = get_restaurant_near_me($request->lat, $request->lng, $where, request()->user()->id, null, null);
             $vendors = $vendors->addSelect('deal_cuisines', 'banner_image')->orderBy('vendors.vendor_ratings', 'desc')->where('vendors.vendor_ratings','>','0')->offset($request->vendor_offset)->limit($request->vendor_limit)->get();
 
-            $vendor_ids = get_active_time_restaurant_ids_near_me($request->lat, $request->lng, $where, false)->toArray(); //not need to pass offset; limit set on products
+           return  $vendor_ids = get_active_time_restaurant_ids_near_me($request->lat, $request->lng, $where, false)->toArray(); //not need to pass offset; limit set on products
             //get productd's shoud display in pagination
             //$products_ids = get_product_with_variant_and_addons(['product_for' => '3'], request()->user()->id, 'products.product_rating', 'desc', true, false, $vendor_ids, $request->product_offset, $request->product_limit);
             
