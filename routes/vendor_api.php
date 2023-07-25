516bcadf3724f85b2943294a70450b1078e1757d
pratyushgaur <33923049+Pratyushgaur@users.noreply.github.com>
2023-07-25 11:23:03 +0530

    vendor api ready to dispatch api

--- routes/vendor_api.php
+++ routes/vendor_api.php
@@ -4,5 +4,6 @@
     Route::post('home', [App\Http\Controllers\api\vendor\HomeApiController::class, 'index']);
     Route::post('accept-order', [App\Http\Controllers\api\vendor\HomeApiController::class, 'order_preparing']);
     Route::post('reject-order', [App\Http\Controllers\api\vendor\HomeApiController::class, 'order_vendor_reject']);
+    Route::post('ready-to-dispatch', [App\Http\Controllers\api\vendor\HomeApiController::class, 'ready_to_dispatch']);
     
 });
